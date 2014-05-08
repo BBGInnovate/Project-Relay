589d164ccf35e3f2c40e30fc2439384f21bf1bb8
Doug Ward <dougward@watersward.com>
2014-05-08 10:49:33 -0400

    Updated Really theme.

--- sites/all/themes/custom/relay/templates/node--relay_live.tpl.php
+++ sites/all/themes/custom/relay/templates/node--relay_live.tpl.php
@@ -77,36 +77,46 @@
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <?php if (!$page): ?>
-      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+    <section>
+        <div class="small-12 column cs_bg_blue card_top_row"> <span class="left spotlight"><i class="fa fa-bolt"></i> <a href="<?php print $node_url; ?>" class="cs_white"><?php print $title; ?></a></span><span class="right"><a data-reveal-id="alert_modal" class="cs_white" href="#">Get Alerts</a> <a data-refresh-events="" class="cs_white" href="#"><i class="fa fa-bell"></i><div data-count="2" id="badge">3</div></a>
+	</span></div>
+    </section>
 
-  <?php if ($display_submitted): ?>
-    <div class="posted">
-      <?php if ($user_picture): ?>
-        <?php print $user_picture; ?>
-      <?php endif; ?>
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
+    <section class="cs_main relay_live">
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_tags']);
+        print render($content);
+        ?>
+    </section>
 
-  <?php
-    // We hide the comments and links now so that we can render them later.
-    hide($content['comments']);
-    hide($content['links']);
-    hide($content['field_tags']);
-    print render($content);
-  ?>
+    <section class="footer-content">
+        <?php print render($title_prefix); ?>
+        <?php if (!$page): ?>
+            <?php if (!$page): ?>
+                <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+            <?php endif; ?>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
 
-  <?php if (!empty($content['field_tags']) && !$is_front): ?>
-    <?php print render($content['field_tags']) ?>
-  <?php endif; ?>
 
-  <?php print render($content['links']); ?>
-  <?php print render($content['comments']); ?>
+        <?php if (!empty($content['field_tags']) && !$is_front): ?>
+            <?php print render($content['field_tags']) ?>
+        <?php endif; ?>
 
+        <div class="card_social light">
+		  <span class="left">
+			  <a href="#"><i class="fa fa-facebook-square"></i></a>
+			  <a href="#"><i class="fa fa-twitter"></i></a>
+			  <a href="#"><i class="fa fa-google-plus-square"></i></a>
+		  </span>
+            <span class="right"><a href="#"><i class="fa fa-comment"></i></a></span>
+        </div>
+        <p class="card_follow_live"><a href="#">Follow our live coverage <i class="fa fa-chevron-circle-right"></i></a></p>
+
+        <!--< ?php print render($content['links']); ?>  Commented out by Rob McIntosh - May 6th-->
+        <!--< ?php print render($content['comments']); ?>  Commented out by Rob McIntosh - May 6th-->
+    </section>
 </article>

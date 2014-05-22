4ad84412cde7a4d78349c22572cd0becf2fa48d0
Doug Ward <dougward@watersward.com>
2014-05-21 23:18:15 -0400

    Updated page.tpl.php; relay.info; relay.css.

--- sites/all/themes/custom/relay/templates/page.tpl.php
+++ sites/all/themes/custom/relay/templates/page.tpl.php
@@ -1,71 +1,87 @@
 <!--.page -->
+<?php
+// Determine Event Name and Event ID
+$nid = 2;
+$node = node_load($nid);
+$result = field_view_field('node', $node, 'relay_events_ref', array('default'));
+$eventName =  $result['#object']->relay_events_ref['und'][0]['taxonomy_term']->name;
+?>
+
+<div class="page-top-bg"></div>
 <div role="document" class="page">
 
-  <!--.l-header region -->
+  <!--.l-header -->
   <header role="banner" class="l-header">
 
     <?php if ($top_bar): ?>
       <!--.top-bar -->
       <?php if ($top_bar_classes): ?>
-      <div class="<?php print $top_bar_classes; ?>">
+        <div class="<?php print $top_bar_classes; ?>">
       <?php endif; ?>
-        <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
-          <ul class="title-area">
-            <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
-            <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
-          </ul>
-          <section class="top-bar-section">
-            <?php if ($top_bar_main_menu) :?>
-              <?php print $top_bar_main_menu; ?>
-            <?php endif; ?>
-            <?php if ($top_bar_secondary_menu) :?>
-              <?php print $top_bar_secondary_menu; ?>
-            <?php endif; ?>
-          </section>
-        </nav>
+      <nav class="top-bar" data-topbar <?php print $top_bar_options; ?>>
+        <ul class="title-area cs_bg_white">
+				<li class="name"><h1><span class="left"><a href="<?php print $front_page; ?>"><img src="/<?php print path_to_theme(); ?>/images/VOA-blue-official.png" class="logo" alt="VOA logo" /></a></span><span class="left cs_white logo-text"><?php print $linked_site_name; ?></span></h1></li>
+          <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
+        </ul>
+			  <section class="top-bar-section cs_bg_white">
+				<ul class="right">
+				  <li><a href=""><i class="fa fa-facebook-square fa-2x"></i></a></li>
+				  <li><a href=""><i class="fa fa-twitter fa-2x"></i></a></li>
+				  <li><a href=""><i class="fa fa-google-plus fa-2x"></i></a></li>
+				  <li class="active"><a href="#"><i class="fa fa-question fa-2x"></i></a></li>
+				</ul>
+			  </section>
+      </nav>
+			<section class="social">
+				<div class="small-12 ticker">
+				    <p><i class="fa fa-twitter"></i> Follow the Conversation with <a href="#"
+                        class="alert
+				    label">#eventHashTag</a></p>
+				</div>
+			</section>
       <?php if ($top_bar_classes): ?>
-      </div>
+        </div>
       <?php endif; ?>
       <!--/.top-bar -->
     <?php endif; ?>
 
     <!-- Title, slogan and menu -->
     <?php if ($alt_header): ?>
-    <section class="row <?php print $alt_header_classes; ?>">
+      <section class="row <?php print $alt_header_classes; ?>">
 
-      <?php if ($linked_logo): print $linked_logo; endif; ?>
+        <?php if ($linked_logo): print $linked_logo; endif; ?>
 
-      <?php if ($site_name): ?>
-        <?php if ($title): ?>
-          <div id="site-name" class="element-invisible">
-            <strong>
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name" class="element-invisible">
+              <strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+              </strong>
+            </div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
               <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </strong>
-          </div>
-        <?php else: /* Use h1 when the content title is empty */ ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+            </h1>
+          <?php endif; ?>
         <?php endif; ?>
-      <?php endif; ?>
 
-      <?php if ($site_slogan): ?>
-        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
-      <?php endif; ?>
+        <?php if ($site_slogan): ?>
+          <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
+        <?php endif; ?>
 
-      <?php if ($alt_main_menu): ?>
-        <nav id="main-menu" class="navigation" role="navigation">
-          <?php print ($alt_main_menu); ?>
-        </nav> <!-- /#main-menu -->
-      <?php endif; ?>
+        <?php if ($alt_main_menu): ?>
+          <nav id="main-menu" class="navigation" role="navigation">
+            <?php print ($alt_main_menu); ?>
+          </nav> <!-- /#main-menu -->
+        <?php endif; ?>
 
-      <?php if ($alt_secondary_menu): ?>
-        <nav id="secondary-menu" class="navigation" role="navigation">
-          <?php print $alt_secondary_menu; ?>
-        </nav> <!-- /#secondary-menu -->
-      <?php endif; ?>
+        <?php if ($alt_secondary_menu): ?>
+          <nav id="secondary-menu" class="navigation" role="navigation">
+            <?php print $alt_secondary_menu; ?>
+          </nav> <!-- /#secondary-menu -->
+        <?php endif; ?>
 
-    </section>
+      </section>
     <?php endif; ?>
     <!-- End title, slogan and menu -->
 
@@ -83,7 +99,7 @@
   <!--/.l-header -->
 
   <?php if (!empty($page['featured'])): ?>
-    <!--/.featured -->
+    <!--.featured -->
     <section class="l-featured row">
       <div class="large-12 columns">
         <?php print render($page['featured']); ?>
@@ -93,7 +109,7 @@
   <?php endif; ?>
 
   <?php if ($messages && !$zurb_foundation_messages_modal): ?>
-    <!--/.l-messages -->
+    <!--.l-messages -->
     <section class="l-messages row">
       <div class="large-12 columns">
         <?php if ($messages): print $messages; endif; ?>
@@ -103,8 +119,8 @@
   <?php endif; ?>
 
   <?php if (!empty($page['help'])): ?>
-    <!--/.l-help -->
-    <section class="l-help row">
+    <!--.l-help -->
+		<section class="l-help row" style="border:1px solid red; overflow:hidden; clear:both;">
       <div class="large-12 columns">
         <?php print render($page['help']); ?>
       </div>
@@ -113,7 +129,7 @@
   <?php endif; ?>
 
   <main role="main" class="row l-main">
-    <div class="<?php print $main_grid; ?> main columns">
+
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlight panel callout">
           <?php print render($page['highlighted']); ?>
@@ -122,7 +138,7 @@
 
       <a id="main-content"></a>
 
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+			<!--/.<?php if ($breadcrumb): print $breadcrumb; endif; ?> -->
 
       <?php if ($title && !$is_front): ?>
         <?php print render($title_prefix); ?>
@@ -142,81 +158,50 @@
       <?php endif; ?>
 
       <?php print render($page['content']); ?>
-    </div>
-    <!--/.main region -->
+
+    <!--/.l-main region -->
 
     <?php if (!empty($page['sidebar_first'])): ?>
-      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar">
+      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> l-sidebar-first columns sidebar">
         <?php print render($page['sidebar_first']); ?>
       </aside>
     <?php endif; ?>
 
     <?php if (!empty($page['sidebar_second'])): ?>
-      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> sidebar-second columns sidebar">
+      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> l-sidebar-second columns sidebar">
         <?php print render($page['sidebar_second']); ?>
       </aside>
     <?php endif; ?>
   </main>
-  <!--/.main-->
+  <!--/.l-main-->
 
-  <?php if (!empty($page['triptych_first']) || !empty($page['triptych_middle']) || !empty($page['triptych_last'])): ?>
-    <!--.triptych-->
-    <section class="l-triptych row">
-      <div class="triptych-first large-4 columns">
-        <?php print render($page['triptych_first']); ?>
-      </div>
-      <div class="triptych-middle large-4 columns">
-        <?php print render($page['triptych_middle']); ?>
-      </div>
-      <div class="triptych-last large-4 columns">
-        <?php print render($page['triptych_last']); ?>
-      </div>
-    </section>
-    <!--/.triptych -->
+  <?php if (!empty($page['footer_first']) || !empty($page['footer_middle']) || !empty($page['footer_last'])): ?>
+    <!--.l-footer-->
+    <footer class="l-footer panel row" role="contentinfo">
+      <?php if (!empty($page['footer_first'])): ?>
+        <div id="footer-first" class="large-4 columns">
+          <?php print render($page['footer_first']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_middle'])): ?>
+        <div id="footer-middle" class="large-4 columns">
+          <?php print render($page['footer_middle']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_last'])): ?>
+        <div id="footer-last" class="large-4 columns">
+          <?php print render($page['footer_last']); ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($site_name) :?>
+        <div class="copyright large-12 columns">
+          &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
+        </div>
+      <?php endif; ?>
+    </footer>
+    <!--/.footer-->
   <?php endif; ?>
-
-  <?php if (!empty($page['footer_firstcolumn']) || !empty($page['footer_secondcolumn']) || !empty($page['footer_thirdcolumn']) || !empty($page['footer_fourthcolumn'])): ?>
-    <!--.footer-columns -->
-    <section class="row l-footer-columns">
-      <?php if (!empty($page['footer_firstcolumn'])): ?>
-        <div class="footer-first large-3 columns">
-          <?php print render($page['footer_firstcolumn']); ?>
-        </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_secondcolumn'])): ?>
-        <div class="footer-second large-3 columns">
-          <?php print render($page['footer_secondcolumn']); ?>
-        </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_thirdcolumn'])): ?>
-        <div class="footer-third large-3 columns">
-          <?php print render($page['footer_thirdcolumn']); ?>
-        </div>
-      <?php endif; ?>
-      <?php if (!empty($page['footer_fourthcolumn'])): ?>
-        <div class="footer-fourth large-3 columns">
-          <?php print render($page['footer_fourthcolumn']); ?>
-        </div>
-      <?php endif; ?>
-    </section>
-    <!--/.footer-columns-->
-  <?php endif; ?>
-
-  <!--.l-footer-->
-  <footer class="l-footer panel row" role="contentinfo">
-    <?php if (!empty($page['footer'])): ?>
-      <div class="footer large-12 columns">
-        <?php print render($page['footer']); ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
-    <?php endif; ?>
-  </footer>
-  <!--/.footer-->
 
   <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
 </div>
